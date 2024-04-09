bb676e6610a362a055ca487d6dda6eba47ca37e5
frun36 <franek.urbanski@gmail.com>
2024-04-09 20:35:41 +0200

    Refactoring

--- CrazyPlots/Linalg.hpp
+++ CrazyPlots/Linalg.hpp
@@ -6,45 +6,55 @@ class Matrix;
 class Vector {
 	friend Matrix;
 private:
-	double data[3];
+	double _data[3];
 public:
 	Vector(double x = 0.0, double y = 0.0) {
-		data[0] = x;
-		data[1] = y;
-		data[2] = 1.0;
+		_data[0] = x;
+		_data[1] = y;
+		_data[2] = 1.0;
 	}
 
-	void setX(double x) { data[0] = x; }
+	void setX(double x) {
+		_data[0] = x;
+	}
 
-	void setY(double y) { data[1] = y; }
+	void setY(double y) {
+		_data[1] = y;
+	}
 
-	double getX() const { return data[0]; }
+	double getX() const {
+		return _data[0];
+	}
 
-	double getY(double y) const { return data[1]; }
+	double getY(double y) const {
+		return _data[1];
+	}
 };
 
 class Matrix {
 private:
-	double data[3][3];
+	double _data[3][3];
 
 public:
 	Matrix() {
-		data[0][0] = 0.0; data[0][1] = 0.0; data[0][2] = 0.0;
-		data[1][0] = 0.0; data[1][1] = 0.0; data[1][2] = 0.0;
-		data[2][0] = 0.0; data[2][1] = 0.0; data[2][2] = 1.0;
+		_data[0][0] = 0.0; _data[0][1] = 0.0; _data[0][2] = 0.0;
+		_data[1][0] = 0.0; _data[1][1] = 0.0; _data[1][2] = 0.0;
+		_data[2][0] = 0.0; _data[2][1] = 0.0; _data[2][2] = 1.0;
 	}
 
 	Matrix operator*(const Matrix& other) const {
 		int i, j, k;
 		Matrix tmp;
 
-		for (i = 0; i < 3; i++)
-			for (j = 0; j < 3; j++)
-			{
-				tmp.data[i][j] = 0.0;
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				tmp._data[i][j] = 0.0;
+
 				for (k = 0; k < 3; k++)
-					tmp.data[i][j] = tmp.data[i][j] + (data[i][k] * other.data[k][j]);
+					tmp._data[i][j] = tmp._data[i][j] + (_data[i][k] * other._data[k][j]);
 			}
+		}
+
 		return tmp;
 	}
 
@@ -52,10 +62,9 @@ public:
 		unsigned int i, j;
 		Vector tmp;
 
-		for (i = 0; i < 3; i++)
-		{
-			tmp.data[i] = 0.0;
-			for (j = 0; j < 3; j++) tmp.data[i] = tmp.data[i] + (data[i][j] * v.data[j]);
+		for (i = 0; i < 3; i++) {
+			tmp._data[i] = 0.0;
+			for (j = 0; j < 3; j++) tmp._data[i] = tmp._data[i] + (_data[i][j] * v._data[j]);
 		}
 		return tmp;
 	}
